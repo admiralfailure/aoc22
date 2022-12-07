4829c95a9c3139023457e4d3f01e5a6547207210
Joe Andrews <joseph.andrews@eploy.co.uk>
2022-12-07 17:08:20 +0000

    Day 7.1

---
+++ 7.1/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	Run()
+}
+
+func handleError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Run() {
+	file, err := os.Open("input.txt")
+	handleError(err)
+
+	defer file.Close()
+
+	var tree TreeNode = &DirNode{Name: "/"}
+	root := tree
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		input := scanner.Text()
+
+		// Process line
+		// Can either be instruction or output
+		if strings.HasPrefix(input, "$") {
+			// If instruction, update our current location accordingly
+			instructionParts := strings.Split(input, " ")
+
+			switch instructionParts[1] {
+			case "cd":
+				switch instructionParts[2] {
+				case "/":
+					tree = root
+					break
+				case "..":
+					tree = tree.GetParent()
+					break
+				default:
+					for _, elm := range tree.Children() {
+						if elm.GetName() == instructionParts[2] {
+							tree = elm
+							break
+						}
+					}
+
+					break
+				}
+				break
+			case "ls":
+				break
+			}
+		} else {
+			// If output, update data accordingly
+			dataParts := strings.Split(input, " ")
+			if dataParts[0] == "dir" {
+				// Directory
+				newNode := DirNode{Name: dataParts[1]}
+				newNode.Parent = tree
+
+				//fmt.Println("Adding child", newNode, "to tree", tree)
+				tree.AddChild(&newNode)
+			} else {
+				// File
+				size, _ := strconv.Atoi(dataParts[0])
+				newNode := FileNode{Name: dataParts[1], FileSize: uint64(size)}
+				newNode.Parent = tree
+
+				tree.AddChild(&newNode)
+			}
+		}
+
+	}
+
+	indent := ""
+
+	fmt.Println(root.Print())
+	for _, elm := range root.Children() {
+		printNode(indent+"  ", elm)
+	}
+
+	fmt.Println("Total valid size:", getTotalSize(root))
+}
+
+func printNode(indent string, node TreeNode) {
+	fmt.Println(indent + node.Print())
+	for _, elm := range node.Children() {
+		printNode(indent+"  ", elm)
+	}
+}
+
+func getTotalSize(node TreeNode) uint64 {
+	var size uint64 = 0
+
+	if len(node.Children()) > 0 && node.Size() <= 100000 {
+		size += node.Size()
+	}
+
+	for _, elm := range node.Children() {
+		childSize := getTotalSize(elm)
+		size += childSize
+	}
+
+	fmt.Println("Getting size for node", node.GetName(), size)
+
+	return size
+}
